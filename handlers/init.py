f4d2f3831863143243834704f52b92193cc95506
SuneraEs <svyat718@gmail.com>
2025-08-19 09:58:11 +0200

    Update init.py

--- handlers/init.py
+++ handlers/init.py
@@ -1,6 +1,16 @@
-# handlers/__init__.py
 from .start import cmd_start, cmd_id, cmd_admin, on_text
-from .lang import lang_menu, lang_pick_cb, cmd_lang, lang_handlers
 from .form import form_conv_handler
 from .credit import credit_conv_handler
 from .solar import solar_conv_handler
+from .lang import lang_handlers
+
+__all__ = [
+    "cmd_start",
+    "cmd_id",
+    "cmd_admin",
+    "on_text",
+    "form_conv_handler",
+    "credit_conv_handler",
+    "solar_conv_handler",
+    "lang_handlers",
+]
